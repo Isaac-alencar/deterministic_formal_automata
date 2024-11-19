02ffc62d897718cbe6d654b3a04efa6ee93e4ddc
Isaac Alencar <alencarisaac3@gmail.com>
2024-11-19 10:46:08 -0300

    implements DFA

--- lib/dfa.ex
+++ lib/dfa.ex
@@ -1,18 +1,38 @@
 defmodule DFA do
-  @moduledoc """
-  Documentation for `DFA`.
-  """
+  def process_input(keyword \\ "") do
+    read_chars(keyword, 0, :initial_state)
+  end
 
-  @doc """
-  Hello world.
+  defp read_chars(_keyword, 2, :accept), do: {:accept}
+  defp read_chars(_keyword, _q0, :reject), do: {:reject}
 
-  ## Examples
+  defp read_chars(keyword, q0, state) do
+    current_char = String.at(keyword, q0)
+    next_state = transite_state(current_char, state)
 
-      iex> DFA.hello()
-      :world
+    read_chars(keyword, q0 + 1, next_state)
+  end
 
-  """
-  def hello do
-    :world
+  defp transite_state(curr_char, :initial_state) do
+    case curr_char do
+      "0" ->
+        :accept
+
+      "1" ->
+        :reject
+
+      _ ->
+        :reject
+    end
+  end
+
+  defp transite_state(char, :accept) do
+    cond do
+      char == "1" ->
+        :accept
+
+      char == "0" ->
+        :reject
+    end
   end
 end
--- test/dfa_test.exs
+++ test/dfa_test.exs
@@ -2,7 +2,21 @@ defmodule DFATest do
   use ExUnit.Case
   doctest DFA
 
-  test "greets the world" do
-    assert DFA.hello() == :world
+  test "rejects input outside aphabet" do
+    assert DFA.process_input("411001") == {:reject}
+    assert DFA.process_input("31001") == {:reject}
+    assert DFA.process_input("211001") == {:reject}
+  end
+
+  test "rejects input if not starts with 01" do
+    assert DFA.process_input("111001") == {:reject}
+    assert DFA.process_input("001001") == {:reject}
+    assert DFA.process_input("101001") == {:reject}
+  end
+
+  test "accept the keyword when its is valid" do
+    assert DFA.process_input("011001") == {:accept}
+    assert DFA.process_input("01001") == {:accept}
+    assert DFA.process_input("011001") == {:accept}
   end
 end
